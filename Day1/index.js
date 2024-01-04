314eb8617b2a6548449fde6a14a84f28d7eb4b99
Badmus Muhammed <akoredeishola10@gmail.com>
2024-01-04 16:26:50 +0000

    ghj

---
+++ Day1/index.js
@@ -0,0 +1,47 @@
+// 1. Arithmetic Operators
+// Arithmetic operators perform mathematical calculations.
+
+let a = 10;
+let b = 5;
+
+let addition = a + b; // Addition (+)
+let subtraction = a - b; // Subtraction (-)
+let multiplication = a * b; // Multiplication (*)
+let division = a / b; // Division (/)
+let modulus = a % b; // Modulus (remainder) (%)
+let increment = a++; // Increment (++) - increases the value by 1
+let decrement = b--; // Decrement (--) - decreases the value by 1
+
+// 2. Comparison Operators
+// Comparison operators compare two values and return a Boolean result (true or false).
+
+let x = 5;
+let y = 10;
+
+console.log(x > y); // Greater than (>)
+console.log(x < y); // Less than (<)
+console.log(x >= y); // Greater than or equal to (>=)
+console.log(x <= y); // Less than or equal to (<=)
+console.log(x === y); // Equal to (strict equality) (===)
+console.log(x !== y); // Not equal to (!==)
+
+// 3. Logical Operators
+// Logical operators perform logical operations on values and return a Boolean result.
+
+let p = true;
+let q = false;
+
+console.log(p && q); // Logical AND (&&)
+console.log(p || q); // Logical OR (||)
+console.log(!p); // Logical NOT (!)
+
+
+// 4. Assignment Operators
+// Assignment operators assign values to variables.
+
+let c = 10;
+
+c += 5; // Equivalent to: c = c + 5;
+c -= 3; // Equivalent to: c = c - 3;
+c *= 2; // Equivalent to: c = c * 2;
+c /= 4; // Equivalent to: c = c / 4;
