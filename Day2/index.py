901cf6c2ca5cd74ea959b7eefb450a240a65b8cf
Badmus Muhammed <akoredeishola10@gmail.com>
2024-01-05 20:28:30 +0000

    dgfh

---
+++ Day2/index.py
@@ -0,0 +1,58 @@
+# Addition (+): Adds two operands.
+
+a = 10
+b = 20
+result = a + b
+print(result)  # Output: 30
+
+
+# Subtraction (-): Subtracts the right operand from the left operand.
+
+a = 30
+b = 15
+result = a - b
+print(result)  # Output: 15
+
+
+# Multiplication (*): Multiplies two operands.
+
+a = 5
+b = 6
+result = a * b
+print(result)  # Output: 30
+
+
+# Division (/): Divides the left operand by the right operand (always results in a float).
+
+a = 20
+b = 3
+result = a / b
+print(result)  # Output: 6.666666666666667
+
+
+# Modulus (%): Returns the remainder when the left operand is divided by the right operand.
+
+a = 20
+b = 3
+result = a % b
+print(result)  # Output: 2
+
+
+# Floor Division (//): Performs division and rounds down to the nearest whole number.
+
+a = 20
+b = 3
+result = a // b
+print(result)  # Output: 6
+
+
+# Exponentiation ()**: Raises the left operand to the power of the right operand.
+
+a = 2
+b = 3
+result = a ** b
+print(result)  # Output: 8
+
+
+
+
